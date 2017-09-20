2b28d3993161fd45f3fc28d1860101ddd335f17d
Matthew Brett <matthew.brett@gmail.com>
2017-09-20 13:18:22 +0100

    Try again installing Pythn 3.6.2

--- install_python362.ps1
+++ install_python362.ps1
@@ -1,16 +1,21 @@
 # Install Python 3.6.2
-Write-Host "Installing Python 3.6.2..." -ForegroundColor Cyan
-Write-Host "Downloading..."
-$exePath = "$env:TEMP\python-3.6.2.exe"
-(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.6.2/python-3.6.2.exe', $exePath)
-Write-Host "Installing..."
-cmd /c start /wait $exePath /quiet TargetDir=C:\Python362 Shortcuts=0 Include_launcher=0 InstallLauncherAllUsers=0
-Write-Host "Python 3.6.2 x86 installed"
+if ($env:PYTHON -eq "C:\Python362-x64") {
+    $exe_suffix="-amd64"
+    $install_suffix="-x64"
+} elseif ($env:PYTHON -eq "C:\Python362") {
+    $exe_suffix=""
+    $install_suffix=""
+} else {
+    exit 0
+}
 
-# Python 3.6.2 x64
-Write-Host "Downloading..."
-$exePath = "$env:TEMP\python-3.6.2-amd64.exe"
-(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.6.2/python-3.6.2-amd64.exe', $exePath)
+Write-Host "Installing Python 3.6.2$exe_suffix..." -ForegroundColor Cyan
+$exePath = "$env:TEMP\python-3.6.2${exe_suffix}.exe"
+$downloadFile = "https://www.python.org/ftp/python/3.6.2/python-3.6.2${exe_suffix}.exe"
+Write-Host "Downloading $downloadFile..."
+(New-Object Net.WebClient).DownloadFile($downloadFile, $exePath)
 Write-Host "Installing..."
-cmd /c start /wait $exePath /quiet TargetDir=C:\Python362-x64 Shortcuts=0 Include_launcher=0 InstallLauncherAllUsers=0
-Write-Host "Python 3.6.2 x64 installed"
+cmd /c start /wait $exePath /quiet TargetDir="C:\Python362$install_suffix$" Shortcuts=0 Include_launcher=0 InstallLauncherAllUsers=0
+Write-Host "Python 3.6.2${install_suffix} installed"
+
+c:\Python36${install_suffix}\Python.exe --version
